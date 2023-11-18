97469bb0b9634bbd682cdefab2f76e0251f7f5c2
Taider <montassartaider002@gmail.com>
2023-11-18 19:45:21 +0100

    Sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import { Island } from "../models/Island";
+import Sky from "../models/Sky";
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
@@ -36,6 +37,7 @@ const Home = () => {
             groundColor="#000000"
             intensity={1}
           />
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
